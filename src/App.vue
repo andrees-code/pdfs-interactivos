08f0049ee337be8adea590e8fcbf5146043868c8
andrees-code <dronmelinton@gmail.com>
2026-04-21 08:30:01 +0200

    analiticas de vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 import { onMounted } from 'vue'
+import { Analytics } from '@vercel/analytics/vue'
 import { useAuthStore } from '@/stores/auth'
 
 const authStore = useAuthStore()
 
 onMounted(() => {
-  // Si hay un token activo al cargar la aplicación, 
+  // Si hay un token activo al cargar la aplicación,
   // buscamos los datos más recientes del usuario en el backend
   if (authStore.isAuthenticated) {
     authStore.refreshUser()
@@ -15,6 +16,7 @@ onMounted(() => {
 
 <template>
   <RouterView />
+  <Analytics />
 </template>
 
 <style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,11 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { inject } from '@vercel/analytics'
 
 import App from './App.vue'
 import router from './router'
 import './assets/variables.css'
 import './assets/global.css'
 import 'leaflet/dist/leaflet.css'
-
-inject()
 
 const app = createApp(App)
 
